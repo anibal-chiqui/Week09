ffa04c7dc86af2bbb9e5d765356ee33211154286
“jy8474np” <jy8474np@go.minnstate.edu>
2020-05-12 22:23:14 -0500

    Student Sign-In in progress. Student Components added.

---
+++ student-sign-in/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import NewStudentForm from './components/NewStudentForm.vue'
+import StudentMessage from './components/StudentMessage.vue'
+import StudentTable from './components/StudentTable.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NewStudentForm,
+    StudentMessage,
+    StudentTable
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
