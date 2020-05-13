8ab5d0e04e430f498a16c25df4ecb8ca69dff298
“jy8474np” <jy8474np@go.minnstate.edu>
2020-05-13 00:30:11 -0500

    Edit Table added

--- student-sign-in/src/components/StudentTable.vue
+++ student-sign-in/src/components/StudentTable.vue
@@ -2,18 +2,27 @@
     <div>
         <div class="card student-list m-2 p-2">
             <h4 class="card-title">Students</h4>
+
+            <div class="edit-table-toggle form-check">
+                <input id="edit-table" type="checkbox" class="form-check-input" v-model="editTable">
+                <label for="edit-table" class="form-check-label">Edit table?</label>
+            </div>
+
             <div id="student-table">
                 <table class="table">
                     <tr>
                         <th>Name</th>
                         <th>StarID</th>
                         <th>Present?</th>
+                        <th v-show="editTable">Delete</th>
                     </tr>
-                    <tr v-for="student in students" v-bind:key="student.name" v-bind:class="'present-'+student.present">
-                        <td>{{student.name}}</td>
-                        <td>{{student.starID}}</td>
-                        <td> <input type="checkbox" v-model="student.present" v-on:change="checked(student)"></td>
-                    </tr>
+
+                    <StudentRow
+                            v-for="student in students" v-bind:key="student.name"
+                            v-bind:student="student"
+                            v-bind:editTable="editTable"
+                            v-on:student-present="studentArrivedOrLeft">
+                    </StudentRow>
                 </table>
             </div>
         </div>
@@ -23,13 +32,21 @@
 
 <script>
 
+    import StudentRow from "./StudentRow";
+
     export default {
         name: 'StudentTable',
+        components: {StudentRow},
+        data() {
+            return {
+                editTable: false
+            }
+        },
         props: {
             students: Array
         },
         methods: {
-            checked(student) {
+            studentArrivedOrLeft(student) {
                 this.$emit('student-present', student)
             }
         }
@@ -38,12 +55,5 @@
 </script>
 
 <style>
-    .present-true {
-        color: gray;
-        font-style: italic;
-    }
 
-    .present-false {
-        font-weight: bold;
-    }
 </style>
